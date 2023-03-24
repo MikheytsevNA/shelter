e100ed7a1ade2b7f69cfcad51f88380d471795e1
Nikita Mikheitsev <welt992@gmail.com>
2023-03-24 10:59:37 +0100

    init: scss files setup

--- shelter/gulpfile.js
+++ shelter/gulpfile.js
@@ -9,13 +9,13 @@ function watch () {
         server: "./"
     });
 
-    gulp.watch("./scss/*.scss", style);
+    gulp.watch("./scss/**/*.scss", style);
     gulp.watch("./*.html").on('change', browserSync.reload);
 };
 
 // Compile sass into CSS & auto-inject into browsers
 function style() {
-    return gulp.src("./scss/*.scss")
+    return gulp.src("./scss/**/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
